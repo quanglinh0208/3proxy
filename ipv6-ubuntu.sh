f864c62eb7667181ee6f4c118b1a4e2cd3b6e1fe
quanglinh0208 <88640514+quanglinh0208@users.noreply.github.com>
2023-06-07 17:22:03 +0700

    Update ipv6-ubuntu.sh

--- ipv6-ubuntu.sh
+++ ipv6-ubuntu.sh
@@ -3,37 +3,39 @@
 IPC=$(curl -4 -s icanhazip.com | cut -d"." -f3)
 IPD=$(curl -4 -s icanhazip.com | cut -d"." -f4)
 INT=$(ls /sys/class/net | grep e)
+
 if [ "$IPC" = "4" ]; then
     IPV6_ADDRESS="2403:6a40:0:40::$IPD:0000"
     PREFIX_LENGTH="64"
-    INTERFACE= $INT
+    INTERFACE="$INT"
     GATEWAY="2403:6a40:0:40::1"
 elif [ "$IPC" = "5" ]; then
     IPV6_ADDRESS="2403:6a40:0:41::$IPD:0000"
     PREFIX_LENGTH="64"
-    INTERFACE= $INT
+    INTERFACE="$INT"
     GATEWAY="2403:6a40:0:41::1"
 elif [ "$IPC" = "244" ]; then
     IPV6_ADDRESS="2403:6a40:2000:244::$IPD:0000"
     PREFIX_LENGTH="64"
-    INTERFACE= $INT
+    INTERFACE="$INT"
     GATEWAY="2403:6a40:2000:244::1"
 else
     IPV6_ADDRESS="2403:6a40:0:$IPC::$IPD:0000"
     PREFIX_LENGTH="64"
-    INTERFACE= $INT
+    INTERFACE="$INT"
     GATEWAY="2403:6a40:0:$IPC::1"
 fi
 
 # Add the IPv6 address to the interface
-ip -6 addr add "$IPV6_ADDRESS/$PREFIX_LENGTH" dev "$INT"
+ip -6 addr add "$IPV6_ADDRESS/$PREFIX_LENGTH" dev "$INTERFACE"
+
 # Set the gateway
-ip -6 route add default via "$GATEWAY" dev "$INT"
+ip -6 route add default via "$GATEWAY" dev "$INTERFACE"
 
 # Enable the interface
-ip link set dev "$INT" up
+ip link set dev "$INTERFACE" up
 cp ipv6-ubuntu.sh /etc/ipv6-ubuntu.sh
-#rm -rf ipv6-ubuntu.sh
+rm -rf ipv6-ubuntu.sh
 chmod +x /etc/ipv6-ubuntu.sh
 tee -a /etc/rc.local <<EOF
 sudo /etc/ipv6-ubuntu.sh
