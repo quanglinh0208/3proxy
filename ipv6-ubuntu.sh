8a295ed525cb62102007ffbf04846d33818dde63
quanglinh0208 <88640514+quanglinh0208@users.noreply.github.com>
2023-06-07 16:32:14 +0700

    Update ipv6-ubuntu.sh

--- ipv6-ubuntu.sh
+++ ipv6-ubuntu.sh
@@ -27,9 +27,11 @@ fi
 
 # Add the IPv6 address to the interface
 ip -6 addr add "$IPV6_ADDRESS/$PREFIX_LENGTH" dev "$INTERFACE"
-
 # Set the gateway
 ip -6 route add default via "$GATEWAY" dev "$INTERFACE"
 
 # Enable the interface
 ip link set dev "$INTERFACE" up
+cp ipv6-ubuntu.sh /etc/
+rm -rf ipv6-ubuntu.sh
+chmod +x /etc/ipv6-ubuntu.sh
